1841da3bc29ffd018dfa54608cceaccfc271b46d
vdduong <vdduong@free.fr>
2014-10-17 15:47:10 +0200

    Update fitting_backbone.py

--- fitting_backbone.py
+++ fitting_backbone.py
@@ -11,43 +11,37 @@ from collections import deque
 from auction_algo import *
 
 
-#name_file = '/1CPZ.mr.txt'
-name_file = '/1G6J.mr.txt' # ubiquitin
 
-nodes, distance_matrix = read_restraints(name_file)
 
-def dispersion_score(u,v,distance_matrix):
-	dispersion_score = 0.0
-	
-	set_u, set_v = set(), set()
-	
-	for node in distance_matrix[u]:
-		if node!=u:
-			set_u.add(node)
-	for node in distance_matrix[v]:
-		if node!=v:
-			set_v.add(node)
-	set_commun = set_u.intersection(set_v)
-	
-	if len(set_commun)==0:
-		try:
-			distance_ = distance_matrix[u][v]
-			dispersion_score = (dispersion_score+1.)/float(len(set_u)*len(set_v))
-			return dispersion_score
-		except KeyError:
-			return dispersion_score
-	else:
-		sum_local = 0.5
-		for node_1 in set_commun:
-			for node_2 in set_commun:
-				try:
-					distance_ = distance_matrix[node_1][node_2]
-					sum_local+=0.5
-				except KeyError:
-					sum_local+=1.
-		dispersion_score = sum_local/float(len(set_u)*len(set_v))
-		return dispersion_score
+dict_cs = {}
+dict_cs['H_2'] = 8.57 # 0.02
+dict_cs['HB2_2'] = 2.21
+dict_cs['HA_1'] = 4.16
+dict_cs['QB_1'] = 1.33
+dict_cs['QG2_41'] = 0.85
+dict_cs['QE_44'] = 7.41
 
-u='HA_2'
-print u, distance_matrix[u]
+dict_cs['H_3'] = 8.72
+dict_cs['HG2_3'] = 2.1
+dict_cs['HE22_2'] = 6.84
+dict_cs['HA_2'] = 4.52
+dict_cs['HA_3'] = 5.35
+dict_cs['HB3_3'] = 1.88
+dict_cs['HB_68'] = 1.69
+dict_cs['HB2_3'] = 1.96
+dict_cs['HE21_2'] = 7.95
+dict_cs['QG2_67'] = 0.78
+
+dict_cs['H_4'] = 9.15
+dict_cs['QG1_41'] = 0.92
+dict_cs['QB_40'] = 1.05
+dict_cs['HB3_4'] = 2.77
+dict_cs['HA_41'] = 4.89
+dict_cs['HB2_4'] = 2.55
+dict_cs['HA_39'] = 5.62
+dict_cs['QG2_42'] = 0.74
+dict_cs['HA_4'] = 5.16
+dict_cs['QD_4'] = 7.07
+
+# 2: GLN, 3: GLU, 4: PHE
 
