4300109113a6fcc2e8b30b3e1c54eb01a7e287b8
vdduong <vdduong@free.fr>
2014-11-13 16:53:04 +0100

    Update hungarian.py

--- hungarian.py
+++ hungarian.py
@@ -1,1 +1,18 @@
 # hungarian algorithm
+class HungarianError(Exception):
+  pass
+
+try:
+  import numpy as np
+except:
+  raise HungarianError("NumPy is not installed.")
+
+class Hungarian:
+  def __init__(self, input_matrix=None, is_profit_matrix=False):
+    '''
+    input matrix is a list of Lists
+    input_matrix is assumed to be a cost matrix unless is_profit_matrix is True
+    '''
+    if input_matrix is not None:
+      my_matrix = np.array(input_matrix)
+      
